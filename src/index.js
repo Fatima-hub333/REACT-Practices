eb63656695e26204d061423148649ca9524d152b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-03 15:58:20 +0500

    custom components

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const page = (
-  <div>
-    <img src="./react-logo.png" width="40px" />
-    <h1>Func Facts About React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+function MyPage() {
+  return (
+    <ol>
+      <li>It's a popular library, so I'll be 
+      able to fit in with the cool kids!</li>
+      <li>I'm more likely to get a job as a developer
+      if I know React</li>
+    </ol>
+  )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(<MyPage />, document.getElementById("root"))
