f484bb4410e67c2df58e6ca0d7fdce5f8e712f24
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-03 21:11:21 +0500

    State in class components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <h1>{this.props.type} Component</h1>
+/**
+ * Challenge: Convert this stateful function component
+ * to a stateful class component. At the end, everything
+ * should work exactly the way it does now.
+ * 
+ * 1. Change the syntax to a class component
+ * 2. Declare state in the class component that
+ *    can hold the value of `count`
+ * 3. Update the add and subtract methods so you won't
+ *    get the error about calling `setState` on undefined
+ * 4. Update the values in the render method to account
+ *    for the changeover to a class component
+ */
+
+export default class App extends React.Component{
+  // const [count, setCount] = React.useState(0)
+  state = {
+    count: 0
+  }
+  
+  add = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+  
+  subtract = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
+    
+render() {
+  return (
+    <div className="counter">
+      <button className="counter--minus" onClick={this.subtract}>–</button>
+      <div className="counter--count">
+        <h1>{this.count}</h1>
+      </div>
+      <button className="counter--plus" onClick={this.add}>+</button>
+    </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,45 +1,5 @@
-import { render } from "@testing-library/react"
 import React from "react"
 import ReactDOM from "react-dom"
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header username="Fatima Zahir"/>
-        <Greeting />
-      </div>
-    )
-  }
-}
-
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <p>Welcome, {this.props.username}!</p>
-      </header>
-    )
-  }
-}
-
-class Greeting extends React.Component {
-  render() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    
-    if (hours < 12) {
-        timeOfDay = "morning"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
-    } else {
-        timeOfDay = "night"
-    }
-    return (
-        <h1>Good {timeOfDay} to you, sir or madam!</h1>
-    )
-  }
-}
+import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
