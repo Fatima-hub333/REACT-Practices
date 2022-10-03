b78c3448d235d8fbc9294e0cbc9f73612bd7ac2d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-03 19:26:21 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import ReactDOM from "react-dom"
 
 function MyPage() {
   return (
-    <ol>
-      <li>It's a popular library, so I'll be 
-      able to fit in with the cool kids!</li>
-      <li>I'm more likely to get a job as a developer
-      if I know React</li>
-    </ol>
+    <div>
+      <header>
+        <nav>
+          <img src="./react-logo.png" width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be 
+        able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer
+        if I know React</li>
+      </ol>
+      <footer>
+        <small>© 2022 last name here development. All rights reserved.</small>
+      </footer>
+    </div>
   )
 }
 
