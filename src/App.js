a182f621f6e139c950d225f61672040ee6585961
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-04 10:44:23 +0500

    Update App,js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
 import React from "react"
 
-/**
- * Challenge: Convert this stateful function component
- * to a stateful class component. At the end, everything
- * should work exactly the way it does now.
- * 
- * 1. Change the syntax to a class component
- * 2. Declare state in the class component that
- *    can hold the value of `count`
- * 3. Update the add and subtract methods so you won't
- *    get the error about calling `setState` on undefined
- * 4. Update the values in the render method to account
- *    for the changeover to a class component
- */
-
-export default class App extends React.Component{
-  // const [count, setCount] = React.useState(0)
-  state = {
-    count: 0
-  }
-  
-  add = () => {
-    this.setState(prevState => ({count: prevState.count + 1}))
-  }
-  
-  subtract = () => {
-    this.setState(prevState => ({count: prevState.count - 1}))
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.add = this.add.bind(this)
+    this.subtract = this.subtract.bind(this)
   }
     
-render() {
-  return (
-    <div className="counter">
-      <button className="counter--minus" onClick={this.subtract}>–</button>
-      <div className="counter--count">
-        <h1>{this.count}</h1>
-      </div>
-      <button className="counter--plus" onClick={this.add}>+</button>
-    </div>
-    )
-  }
+    
+    add = () => {
+        this.setState(prevState => ({count: prevState.count + 1}))
+    }
+    
+    subtract = () => {
+        this.setState(prevState => ({count: prevState.count - 1}))
+    }
+    
+    render() {
+        return (
+            <div className="counter">
+                <button className="counter--minus" onClick={this.subtract}>–</button>
+                <div className="counter--count">
+                    <h1>{this.state.count}</h1>
+                </div>
+                <button className="counter--plus" onClick={this.add}>+</button>
+            </div>
+        )
+    }
 }
