ef3706f920180f81459494f41a69b0cb557871cc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-04 12:23:16 +0500

    update files

---
+++ src/CTA.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+function CTA() {
+  return (
+    <div className='border'>
+      <h1>This is an important CTA</h1>
+      <button>Click me now or you'll miss out!</button>
+    </div>
+  )
+}
+
+export default CTA
