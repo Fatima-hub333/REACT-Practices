185ff345ad66833b3da8e589ed946bf9db22990e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-07 15:27:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,74 +1,9 @@
-import React, {useState, useEffect} from "react"
-
-/**
- * Challenge:
- * 
- * When the timer reaches 0, count the number of words the user typed in 
- * and display it in the "Word count" section
- * 
- * After the game ends, make it so the user can click the Start button again
- * to play a second time
- */
+import React from 'react'
 
 function App() {
-    const STARTING_TIME = 5
-    
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-    
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-    }
-    
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-    
-    // https://www.google.com/search?q=Disable+button+in+react
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning])
-    
-    return (
-        <div>
-            <h1>How fast do you type?</h1>
-            <textarea
-                onChange={handleChange}
-                value={text}
-                disabled={!isTimeRunning}
-            />
-            <h4>Time remaining: {timeRemaining}</h4>
-            <button 
-                onClick={startGame}
-                disabled={isTimeRunning}
-            >
-                Start
-            </button>
-            <h1>Word count: {wordCount}</h1>
-        </div>
-    )
+  return (
+    <div>Code Goes here</div>
+  )
 }
 
 export default App
