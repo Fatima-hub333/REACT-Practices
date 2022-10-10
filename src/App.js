ea829df6dda26b05e6a29f493f8488ee1b0d5ec2
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-10 10:26:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Link, Switch, Route } from 'react-router-dom'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import Profile from "./pages/profile/profile"
+
+import {Switch, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div>Code Goes here</div>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/profile">
+          <Profile  />
+        </Route>
+
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App"
 
 
 ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-  document.getElementById("root"))
+  <Router>
+    <App />
+  </Router>,
+    document.getElementById("root"))
