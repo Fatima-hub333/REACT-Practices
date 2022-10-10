62edb9b6e8c51476b247f0c7d0fbbcbc63e81eab
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-10-10 11:09:14 +0500

    Add basic functionality in each files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from 'react'
 import { Link, Switch, Route } from 'react-router-dom'
 import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import Profile from "./pages/profile/profile"
-
-import {Switch, Route} from "react-router-dom"
+import Cart from './pages/Cart'
+import Photos from './pages/Photos'
 
 function App() {
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route path="/profile">
-          <Profile  />
-        </Route>
-
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </Switch>
+      <Header />
+      <h1>Home Page</h1>
     </div>
   )
 }
---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <main className="cart-page">
+      <h1>Check Out</h1>
+    </main>
+  )
+}
+
+export default Cart
